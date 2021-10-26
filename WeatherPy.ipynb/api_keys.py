c56a0f964afa086ff643788990143bdb751c87b7
Shannonht23 <89876756+Shannonht23@users.noreply.github.com>
2021-10-26 00:32:23 -0400

    Delete api_keys.py

--- WeatherPy.ipynb/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-#weather_api_key = "a534a743993fa52a6e178f6965432e78"
-weather_api_key = "8f35a737051a72d85ce916dc755f2a7e"
-# Google API Key
-g_key = "AIzaSyDgkVjy6Mc3I5HkNdq7X7jyNq7y3QQ1qYU"
